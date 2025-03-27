59c71bb460dbb1d7975eac3ddd75c9e9041692ae
Ggoojae <jeayoon0@naver.com>
2025-03-27 15:41:13 +0900

    알림, 계정 popover 구현

--- webapp/controller/common/NavigationBar.controller.js
+++ webapp/controller/common/NavigationBar.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Popover",
+    "sap/m/List",
+    "sap/m/StandardListItem",
+    "sap/m/Button"
+], function (Controller, Popover, List, StandardListItem, Button) {
     "use strict";
 
     return Controller.extend("capstone.controller.common.NavigationBar", {
@@ -14,35 +18,56 @@ sap.ui.define([
         },
   
         onBellIconPress: function (oEvent) {
-          var oButton = oEvent.getSource();
-  
-          if (!this.pPopover) {
-              this.pPopover = new Popover({
-                  title: "사용자 정보",
-                  placement: "Bottom",
-                  content: new List({
-                      items: [
-                          new StandardListItem({ title: "최근 활동" }),
-                          new StandardListItem({ title: "자주 사용하는 기능" }),
-                          new StandardListItem({ title: "설정" }),
-                          new StandardListItem({ title: "정보" }),
-                          new StandardListItem({ title: "로그아웃" })
-                      ]
-                  }),
-                  footer: new Button({
-                      text: "닫기",
-                      press: function () {
-                          this.pPopover.close();
-                      }.bind(this)
-                  })
-              });
-              this.getView().addDependent(this.pPopover);
-          }
-          this.pPopover.openBy(oButton);
+            var oButton = oEvent.getSource();
+            var oView = this.getView();
+
+            // Bell 전용 Popover 생성
+            if (!this._oBellPopover) {
+                this._oBellPopover = new Popover({
+                    title: "최근 알림",
+                    placement: "Bottom",
+                    content: new List({
+                        items: [
+                            new StandardListItem({ title: "알림1" }),
+                            new StandardListItem({ title: "알림2" }),
+                            new StandardListItem({ title: "알림3" }),
+                            new StandardListItem({ title: "알림4" }),
+                            new StandardListItem({ title: "알림5" })
+                        ]
+                    })
+                });
+
+                // 뷰에 종속성 추가
+                oView.addDependent(this._oBellPopover);
+            }
+
+            // Bell Popover 열기
+            this._oBellPopover.openBy(oButton);
         },
   
-        onAccountIconPress: function () {
-            alert("계정 아이콘 클릭!");
+        onAccountIconPress: function (oEvent) {
+            var oButton = oEvent.getSource();
+            var oView = this.getView();
+
+            // Account 전용 Popover 생성
+            if (!this._oAccountPopover) {
+                this._oAccountPopover = new Popover({
+                    title: "사용자 정보",
+                    placement: "Bottom",
+                    content: new List({
+                        items: [
+                            new StandardListItem({ title: "정보" }),
+                            new StandardListItem({ title: "설정" }),
+                            new StandardListItem({ title: "로그아웃" })
+                        ]
+                    })
+                });
+                // 뷰에 종속성 추가
+                oView.addDependent(this._oAccountPopover);
+            }
+
+            // Account Popover 열기
+            this._oAccountPopover.openBy(oButton);
         },
     });
 });
