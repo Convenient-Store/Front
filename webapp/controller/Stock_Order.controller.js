64d4abb14dc48ebd3eb40f6092da11e4a374b88b
Ggoojae <jeayoon0@naver.com>
2025-04-08 15:36:10 +0900

    back 구현전 저장

--- webapp/controller/Stock_Order.controller.js
+++ webapp/controller/Stock_Order.controller.js
@@ -1,21 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ], function (Controller) {
     "use strict";
 
-    return Controller.extend("capstone.controller.Stock_Order", {
-        onHomeIconPress: function () {
-            var oRouter = this.getOwnerComponent().getRouter();
-                oRouter.navTo("Launchpad")
-        },
-        onSearchIconPress: function () {
-            alert("검색 아이콘 클릭!");
-        },
-        onBellIconPress: function () {
-            alert("알림 아이콘 클릭!");
-        },
-        onAccountIconPress: function () {
-            alert("계정 아이콘 클릭!");
-        },
-    });
-});
+    return Controller.extend("capstone.controller.Stock_Order.controller", {
+        onInit: function () {
+            var oODataModel = this.getOwnerComponent().getModel();
+            var sEntitySetPath = "/zcap_itemsSet"; // Set 이름 업데이트
+      
+            oODataModel.read(sEntitySetPath, {
+              success: function (oData) {
+                var oViewModel = new JSONModel(oData.results);
+                this.getView().setModel(oViewModel, "stockData");
+              }.bind(this),
+              error: function (oError) {
+                console.error("데이터 로딩 실패", oError);
+              }
+            });
+          },
+      
+          // ... 기타 로직
+        });
+      });
